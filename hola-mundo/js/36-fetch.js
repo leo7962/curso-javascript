24b6df56195fd2fd97e4b1edc2b9f4a6a451f998
leo7962 <leo9622gmail.com>
2019-03-19 23:59:02 -0500

    Se crea una pagina HTML y un nuevo JS

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -1,5 +1,24 @@
 'use strict'
 
 //Fetch (AJAX) y peticiones a servicios / apis rest
+var div_usuarios = document.querySelector("#usuarios");
+var usuarios = [];
 
 fetch('https://jsonplaceholder.typicode.com/users')
+  .then(data => data.json())
+  .then(data => {
+    usuarios = data;
+    console.log(usuarios);
+
+    usuarios.map((user, i) =>{
+      let nombre = document.createElement('h3');
+
+      nombre.innerHTML = i + '. '+ user.name + " Username:" + user.username;
+
+      div_usuarios.appendChild(nombre);
+
+      document.querySelector(".loading").style.display ='none';
+
+    });
+
+  });
