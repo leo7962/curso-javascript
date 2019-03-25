ea9f1ec0b0cd5683a60286a7b59dbd661496a3e2
Leonardo Fabián Hernández Peña <leonardohernandez.fp@academia.umb.edu.co>
2019-03-24 22:34:43 -0500

    Se realizan modificaciones y se crea la carpeta para aprender JQuery

---
+++ aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,14 @@
+'use strict'
+
+$(document).ready(function(){
+//Selector de ID
+  $("#rojo").css("background","red")
+            .css("color","white");
+
+  $("#amarillo").css("background","yellow")
+                .css("color","black");
+
+  $("#verde").css("background", "green")
+             .css("color", "white");
+
+});
--- hola-mundo/js/37-try-catch.js
+++ hola-mundo/js/37-try-catch.js
@@ -1,10 +1,12 @@
 'use strict'
 try {
   var year = 2019;
-  alert(yea);
+  alert(year);
 
-  
+  console.log(decodeURIComponent("https://www.youtube.com/watch?v=ypKv1Q3kj1c"));
+
 } catch (error) {
+  console.log(error);
   alert("Ha ocurrido un error en el codigo")
 } finally {
 
