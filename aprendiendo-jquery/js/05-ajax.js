5a1d86bf2ef25eb1c277d9cbf95323ff0f9dbd32
leo7962 <leo9622gmail.com>
2019-04-10 00:57:22 -0500

    Se crea peticiones AJAX con JQuery

---
+++ aprendiendo-jquery/js/05-ajax.js
@@ -0,0 +1,44 @@
+$(document).ready(function functionName() {
+  //Load
+  //$('#datos').load('https://reqres.in');
+
+  //Get
+  $.get('https://reqres.in/api/users',{page: 3}, function (response) {
+    response.data.forEach((element, index) =>{
+        $('#datos').append("<p>"+element.first_name+" "+element.las_name+"</p>");
+    });
+  });
+
+  //Post
+
+  $("#formulario").submit(function(e) {
+    e.preventDefault();
+    var usuario = {
+      "name": $('input[name="name"]').val(),
+      "web": $('input[name="web"]').val()
+    };
+
+    // $.post($(this).attr("action"), usuario, function (response) {
+    //   console.log(response);
+    // }).done(function() {
+    //   alert("Cargado el usuario")
+    // });
+
+    $.ajax({
+      type: 'post',
+      url: $(this).attr("action"),
+      data: usuario,
+      beforeSend: function () {
+        console.log("Enviando usuario...");
+      },
+      success: function(response) {
+        console.log(response);
+      },
+      error: function() {
+        console.log("Ha ocurrido un error");
+      },
+      timeout:2000,
+    });
+    return false;
+  });
+});
