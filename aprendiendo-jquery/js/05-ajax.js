29c8d9424be0c0d768edd201932c34fa72f6e951
leo7962 <leo9622gmail.com>
2019-04-12 00:14:03 -0500

    Aprendiendo estilos con JQuery UI

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -1,11 +1,13 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //Load
   //$('#datos').load('https://reqres.in');
 
   //Get
-  $.get('https://reqres.in/api/users',{page: 3}, function (response) {
-    response.data.forEach((element, index) =>{
-        $('#datos').append("<p>"+element.first_name+" "+element.last_name+"</p>");
+  $.get('https://reqres.in/api/users', {
+    page: 3
+  }, function(response) {
+    response.data.forEach((element, index) => {
+      $('#datos').append("<p>" + element.first_name + " " + element.last_name + "</p>");
     });
   });
 
@@ -28,7 +30,7 @@ $(document).ready(function () {
       type: 'post',
       url: $(this).attr("action"),
       data: usuario,
-      beforeSend: function () {
+      beforeSend: function() {
         console.log("Enviando usuario...");
       },
       success: function(response) {
@@ -37,7 +39,7 @@ $(document).ready(function () {
       error: function() {
         console.log("Ha ocurrido un error");
       },
-      timeout:2000,
+      timeout: 2000,
     });
     return false;
   });
---
+++ aprendiendo-jquery/js/06-interface.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  //Mover elementos por la pagina
+  $('.elemento').draggable();
+
+  //Redimensionar elementos
+  $('.elemento').resizable();
+
+  //seleccionar elementos y ordenar elementos
+  //$('.lista-seleccionable').selectable();
+  $('.lista-seleccionable').sortable({
+    update: function(event, ui) {
+      console.log("Ha cambiado la lista");
+    }
+  });
+  //drop
+  $('#elemento-movido').draggable();
+  $('#area').droppable({
+    drop: function() {
+      console.log("Has soltado algo dentro");
+    }
+  });
+
+  //efectos
+  $('#mostrar').click(function() {
+    //$('.caja-efectos').fadeToggle('slow/400/fast', function() {
+    //});
+    //$('.caja-efectos').toggle("explode");
+    //$('.caja-efectos').toggle("blind");
+    //$('.caja-efectos').toggle("slide");
+    //$('.caja-efectos').toggle("drop");
+    //$('.caja-efectos').toggle("fold");
+    //$('.caja-efectos').toggle("puff");
+    //$('.caja-efectos').toggle("scale");
+    $('.caja-efectos').toggle("shake",4000);
+  });
+});
