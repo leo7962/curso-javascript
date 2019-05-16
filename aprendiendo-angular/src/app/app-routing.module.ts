ef8c4c50511eac6a17f043025deee52d62dfdf75
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-05-16 13:53:09 -0500

    Se hace los principios para enrutamiento paginas.

--- aprendiendo-angular/src/app/app-routing.module.ts
+++ aprendiendo-angular/src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//importar componentes propios o creados
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from "./videojuego/videojuego.component";
+import { CursosComponent } from "./cursos/cursos.component";
+
+//Array de Rutas que se crean por defecto
+
 const routes: Routes = [];
 
 @NgModule({
