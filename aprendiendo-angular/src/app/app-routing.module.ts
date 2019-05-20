6a127bdd1bded16c3af3b5e422030859407e793a
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-05-20 00:40:20 -0500

    se aprende PIPES de Angular

--- aprendiendo-angular/src/app/app-routing.module.ts
+++ aprendiendo-angular/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 //importar componentes propios o creados
 import { HomeComponent } from './home/home.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
-import { VideojuegoComponent } from "./videojuego/videojuego.component";
-import { CursosComponent } from "./cursos/cursos.component";
-import { ExternoComponent } from "./externo/externo.component";
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ExternoComponent } from './externo/externo.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 //Array de Rutas que se crean por defecto
 
@@ -18,6 +19,7 @@ const routes: Routes = [
   {path: 'cursos/', component: CursosComponent },
   {path: 'cursos/:nombre/:followers', component: CursosComponent},
   {path: 'externo', component: ExternoComponent},
+  {path: 'contacto', component: ContactoComponent},
   {path: '**', component: HomeComponent}
 ];
 
