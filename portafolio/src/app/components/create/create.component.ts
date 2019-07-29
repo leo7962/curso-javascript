3f020c2372497d68a1b7eaa9f9d9db7e5183a0c3
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-07-28 23:22:52 -0500

    se deja hasta la imagen, necesita verificación

--- portafolio/src/app/components/create/create.component.ts
+++ portafolio/src/app/components/create/create.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from "../../models/project";
+import { ProjectService } from "../../services/project.service";
+import { UploadService } from "../../services/upload.service";
+import { Global } from "../../services/global";
+
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project;
+  public status: string;
+  public filesToUpload: Array<File>;
+
+
+  constructor(
+    private _ProjectService: ProjectService,
+    private _uploadService: UploadService,
+  ) { 
+    this.title = "Crear Proyecto";
+    this.project = new Project('','','','',2019,'','');
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(form){
+    
+    //Guardar los datos básicos
+    this._ProjectService.saveProject(this.project).subscribe(
+      response => {
+        if(response.project){
+         
+
+          //Subir la imagen a Mongo
+          this._uploadService.makeFileRequest(Global.url+"upload-image"+response.project._id, [], this.filesToUpload, 'image').then((result:any) =>{
+            this.status = 'success';
+            console.log(result);   
+            form.reset();   
+          });
+
+          
+        }else{
+          this.status ='failed';
+        }
+        
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
 }
