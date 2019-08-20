bdce11a981b89cd876d401747fd753a0a82277a3
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-08-20 12:39:18 -0500

    Se crea una lista de proyectos.

--- proyecto-angular/src/app/services/global.service.ts
+++ proyecto-angular/src/app/services/global.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GlobalService {
-  url: 'http://localhost:3700/api'
+  url: 'http://localhost:3700/api';
   constructor() { }
 }
--- proyecto-angular/src/app/services/project.service.service.ts
+++ proyecto-angular/src/app/services/project.service.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { Project } from '../models/project';
 import { GlobalService } from '../services/global.service';  
 
+
 @Injectable()
 export class ProjectService{
   public url: string;
@@ -15,5 +16,11 @@ export class ProjectService{
   }
   testService(){
     return "Probando el servicio de angular";
+  }
+  saveProject(project: Project): Observable<any>{
+    let params = JSON.stringify(project);
+    let headers = new HttpHeaders().set('Content-Type','application/json');
+
+    return this._http.post(this.url+ 'save-project', params, {headers: headers});
   }
 }
